d69a05d2d5602402dd63a9f53e2617afbf162a34
MikeChunko <chunkomike@gmail.com>
2021-04-27 12:40:56 -0400

    Home page now pulls from user's friends

--- routes/home.js
+++ routes/home.js
@@ -10,31 +10,31 @@
 const express = require("express"),
       router = express.Router(),
       data = require("../data")
+      userData = data.users,
       userPets = data.userPets,
       storeFood = data.storeFood,
       config = require("../config.json"),
-      maxFavoritesDisplay = config.maxFavoritePetsDisplay,
-      maxInventoryDisplay = config.maxInventoryDisplay;
 
 // Temporary route faking for debug purposes
 router.get("/", async (req, res) => {
   // Fetch user's favorite pets and limit to the configured setting
-  let favorites = req.session.user.favoritePets.slice(0, maxFavoritesDisplay);
+  let favorites = req.session.user.favoritePets.slice(0, config.maxFavoritesDisplay);
 
   for (let i = 0; i < favorites.length; i++)
     favorites[i] = await userPets.get(favorites[i]);
 
   // Fetch user's inventory and limit to the configured setting
   let inventory = req.session.user.foods,
-      inventoryKeys = Object.keys(inventory).slice(0, maxInventoryDisplay);
+      inventoryKeys = Object.keys(inventory).slice(0, config.maxInventoryDisplay);
 
   for (let i = 0; i < inventoryKeys.length; i++)
     inventoryKeys[i] = await storeFood.get(inventoryKeys[i]);
 
-  // In reality friends will contains user objects
-  // I think only _id and displayname are needed though
-  let friends = [{displayName: "babbis"}, {displayName: "babbis"}, {displayName: "babbis"}, {displayName: "babbis"}];
-  friends = friends.slice(0, 20);
+  // Fetch user's friends and limit to the configured settings
+  let friends = req.session.user.friends.slice(0, config.maxFriendsDisplay);
+
+  for (let i = 0; i < friends.length; i++)
+    friends[i] = await userData.get(friends[i]);
 
   res.render("mojipets/home", {
     title: "MojiPets",
