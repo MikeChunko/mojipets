021b007fffd948d446b5046da2f2b27e2a238e10
MikeChunko <chunkomike@gmail.com>
2021-04-28 14:32:21 -0400

    Switch shop tabs functionality

--- public/shop.js
+++ public/shop.js
@@ -8,23 +8,26 @@
 */
 
 (function($) {
+  var foodTab = $('#food-tab'),
+      petsTab = $('#pets-tab'),
+      shopTitle = $('#shop-title'),
+      shopPets = $('#shop-pets'),
+      shopFood = $('#shop-food');
 
-  var foodTab = $('#food-tab');
-  var petsTab = $('#pets-tab');
-  var shopTitle = $('#shop-title');
-
-  $(foodTab).click(function(e) {
-    // TODO: Ajax request to food route
-    $(foodTab).attr("clicked", "y");
-    $(petsTab).attr("clicked", "n");
-    $(shopTitle).text("Food Items");
+  foodTab.click(function(e) {
+    foodTab.attr("clicked", "y");
+    petsTab.attr("clicked", "n");
+    shopTitle.text("Food Items");
+    shopFood.removeClass("hidden-div");
+    shopPets.addClass("hidden-div");
   });
 
-  $(petsTab).click(function(e) {
-    // TODO: Ajax request to pets route
-    $(petsTab).attr("clicked", "y");
-    $(foodTab).attr("clicked", "n");
-    $(shopTitle).text("New Pets");
+  petsTab.click(function(e) {
+    petsTab.attr("clicked", "y");
+    foodTab.attr("clicked", "n");
+    shopTitle.text("New Pets");
+    shopPets.removeClass("hidden-div");
+    shopFood.addClass("hidden-div");
   });
 
 })(window.jQuery);
