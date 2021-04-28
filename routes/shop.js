52ddc0049ae5ac850c2d3812060736809aebd084
MikeChunko <chunkomike@gmail.com>
2021-04-28 14:54:46 -0400

    Use db for shop, better control of db faults

--- routes/shop.js
+++ routes/shop.js
@@ -13,9 +13,7 @@ const express = require("express"),
       storePets = data.storePets,
       storeFood = data.storeFood;
 
-router.get("/", (req, res) => {
-  // TODO: Fill with real data
-
+router.get("/", async (req, res) => {
   // ! Debug so I don't need to log in each time
   let credits;
   try {
@@ -24,38 +22,21 @@ router.get("/", (req, res) => {
     credits = 1000;
   }
 
-  let shopPets = [{emoji: {name: "cat", img: "/public/resources/pets/cat.svg"}, price: 10},
-  {emoji: {name: "cat", img: "/public/resources/pets/bear.svg"}, price: 11},
-  {emoji: {name: "cat", img: "/public/resources/pets/giraffe.svg"}, price: 12},
-  {emoji: {name: "cat", img: "/public/resources/pets/dog.svg"}, price: 13},
-  {emoji: {name: "cat", img: "/public/resources/pets/cow.svg"}, price: 14},
-  {emoji: {name: "cat", img: "/public/resources/pets/cat.svg"}, price: 15},
-  {emoji: {name: "cat", img: "/public/resources/pets/cat.svg"}, price: 16},
-  {emoji: {name: "cat", img: "/public/resources/pets/cat.svg"}, price: 17},
-  {emoji: {name: "cat", img: "/public/resources/pets/cat.svg"}, price: 18},
-  {emoji: {name: "cat", img: "/public/resources/pets/cat.svg"}, price: 19},
-  {emoji: {name: "cat", img: "/public/resources/pets/cat.svg"}, price: 20}];
+  try {
+    const shopPets = await storePets.getAll(),
+          shopFood = await storeFood.getAll();
 
-  let shopFood = [{emoji: {name: "cat", img: "/public/resources/food/bagel.svg"}, price: 10},
-  {emoji: {name: "cat", img: "/public/resources/food/donut.svg"}, price: 11},
-  {emoji: {name: "cat", img: "/public/resources/food/falafel.svg"}, price: 12},
-  {emoji: {name: "cat", img: "/public/resources/food/mango.svg"}, price: 13},
-  {emoji: {name: "cat", img: "/public/resources/food/meat_on_bone.svg"}, price: 14},
-  {emoji: {name: "cat", img: "/public/resources/food/melon.svg"}, price: 15},
-  {emoji: {name: "cat", img: "/public/resources/food/onigiri.svg"}, price: 16},
-  {emoji: {name: "cat", img: "/public/resources/food/pie.svg"}, price: 17},
-  {emoji: {name: "cat", img: "/public/resources/food/pizza.svg"}, price: 18},
-  {emoji: {name: "cat", img: "/public/resources/food/sushi.svg"}, price: 19},
-  {emoji: {name: "cat", img: "/public/resources/food/bagel.svg"}, price: 20}];
-
-  res.render("mojipets/shop", {
-    title: "MojiPets",
-    shopPets: shopPets,
-    shopFood: shopFood,
-    money: credits,
-    css: "/public/site.css",
-    postjs: "/public/shop.js"
-  });
+    res.render("mojipets/shop", {
+      title: "MojiPets",
+      shopPets: shopPets,
+      shopFood: shopFood,
+      money: credits,
+      css: "/public/site.css",
+      postjs: "/public/shop.js"
+    });
+} catch (e) {  // Some error has occured in the db
+  res.status(500).sendFile(path.resolve("static/error_db.html"));
+}
 });
 
 module.exports = router;
