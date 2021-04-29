931c55f597b74eb8c293ec1f822fbf9c60eecd6b
MyriaCore <development@myriaco.re>
2021-04-29 18:25:12 -0400

    Added unimplemented userpet routes as TODOs

--- routes/api/pet.js
+++ routes/api/pet.js
@@ -11,4 +11,29 @@ const express = require("express"),
       router = express.Router(),
       data = require("../../data");
 
+router.get('/:id', async (req, res) => {
+  res.sendStatus(500).json({ error: 'TODO: implement' })
+})
+
+
+router.put('/:id', async (req, res) => {
+  res.sendStatus(500).json({ error: 'TODO: implement' })
+})
+
+router.patch('/:id', async (req, res) => {
+  res.sendStatus(500).json({ error: 'TODO: implement' })
+})
+
+router.delete('/:id', async (req, res) => {
+  res.sendStatus(500).json({ error: 'TODO: implement' })
+})
+
+router.post('/:id/interactions/feed', async (req, res) => {
+  res.sendStatus(500).json({ error: 'TODO: implement' })
+})
+
+router.post('/:id/interactions/fetch', async (req, res) => {
+  res.sendStatus(500).json({ error: 'TODO: implement' })
+})
+
 module.exports = router
