c6a6da2ce1b9b481c554c5256ebf1d87e084eab0
Elijah Wendel <ezwendel@gmail.com>
2021-05-03 19:59:53 -0400

    deleted extra function

--- data/userPets.js
+++ data/userPets.js
@@ -392,31 +392,6 @@ async function getAge(id) {
   return age;
 }
 
-async function getHappiness(id) {
-  if (!id) throw 'Error: must provide an id.'
-  if (typeof(id) != "string") throw 'Error: type of id not string.'
-  if (id.trim().length == 0) throw 'Error: id is either an empty string or just whitespace.'
-  let pet = null
-  try {
-    pet = await get(id)
-  } catch (e) {
-    throw e
-  }
-  let owner = null
-  try {
-    owner = await getOwner(id)
-  } catch (e) {
-    throw e
-  }
-  let currentDateTimestamp = new Date().getTime()
-  let lastPlayTime = pet.happiness.getTime()
-  if (lastPlayTime + (3 * 24 * 60 * 60 * 1000) >= currentDateTimestamp) { return "Happy" }
-  if (lastPlayTime + (6 * 24 * 60 * 60 * 1000) >= currentDateTimestamp) { return "Content" }
-  if (lastPlayTime + (8 * 24 * 60 * 60 * 1000) >= currentDateTimestamp) { return "Unhappy" }
-  if (lastPlayTime + (9 * 24 * 60 * 60 * 1000) >= currentDateTimestamp) { return "Miserable" }
-  return "Depressed"
-}
-
 async function killPet(id) { // for testing
   if (!id) throw 'Error: must provide an id.'
   if (typeof(id) != "string") throw 'Error: type of id not string.'
