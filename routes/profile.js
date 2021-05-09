4472be59ec75642474d28280a731964732ffd3ce
MikeChunko <chunkomike@gmail.com>
2021-05-09 11:59:43 -0400

    Show most interacted with on profile

--- routes/profile.js
+++ routes/profile.js
@@ -14,7 +14,8 @@ const express = require("express"),
   data = require("../data"),
   userData = data.users,
   config = require("../config.json"),
-  { dateToMMDDYY } = require("../util");
+  { dateToMMDDYY } = require("../util"),
+  cloneDeep = require("lodash.clonedeep");
 
 router.get("/:uname", async (req, res) => {
   let uname = req.params.uname;
@@ -43,7 +44,7 @@ router.get("/:uname", async (req, res) => {
       });
     }
 
-    pets = userObj.favoritePets.slice(0, config.maxFavoritePetsDisplay);
+    pets = cloneDeep(userObj.favoritePets);
 
     // Map favorite pet IDs to the full pet
     for (let i = 0; i < pets.length; i++) {
@@ -61,7 +62,6 @@ router.get("/:uname", async (req, res) => {
       join_date: dateToMMDDYY(userObj.joinDate),
       last_online: dateToMMDDYY(userObj.lastLogin),
       fav_pets: pets,
-      max_pets: config.maxFavoritePetsDisplay,
       css: "/public/site.css"
     });
   } catch (e) {  // Some db error has occurred
@@ -97,7 +97,7 @@ router.get("/:uname/all", async (req, res) => {
       });
     }
 
-    let pets = (await userPets.getPetsFromUser(userObj._id));
+    let pets = await userPets.getPetsFromUser(userObj._id);
 
     // Map happiness, health, and age
     for (let i = 0; i < pets.length; i++) {
@@ -114,6 +114,65 @@ router.get("/:uname/all", async (req, res) => {
       join_date: dateToMMDDYY(userObj.joinDate),
       last_online: dateToMMDDYY(userObj.lastLogin),
       pets: pets,
+      css: "/public/site.css"
+    });
+  } catch (e) {  // Some db error has occurred
+    console.log(e);
+    res.status(500).sendFile(path.resolve("static/error_db.html"));
+  }
+});
+
+router.get("/:uname/most", async (req, res) => {
+  let uname = req.params.uname;
+
+  if (!uname || typeof (uname) != "string")
+    res.sendStatus(404);
+
+  uname = uname.toLowerCase();
+
+  try {
+    const users = await userData.getAllUsers();
+
+    // Search for our desired user
+    const userObj = users.find((user, i) => {
+      if (user.username.toLowerCase() == uname)
+        return user;
+    });
+
+    // User with the given username does not exist
+    if (typeof (userObj) == "undefined") {
+      return res.render("mojipets/profile_most", {
+        title: uname + "'s Profile",
+        username: uname,
+        error: true,
+        css: "/public/site.css"
+      });
+    }
+
+    // Arbitrarily limit to 6
+    let pets = (await userPets.getPetsFromUser(userObj._id)).slice(0, 6);
+
+    // Map happiness, health, agel and total interactions
+    for (let i = 0; i < pets.length; i++) {
+      pets[i].happiness = await userPets.getHappiness(pets[i]._id);
+      pets[i].status = await userPets.getStatus(pets[i]._id);
+      pets[i].age = await userPets.getAge(pets[i]._id);
+      pets[i].interactions = await userPets.getTotalInteractions(pets[i]._id)
+    }
+
+    // Sort based on interactions
+    pets.sort((a, b) => {
+      return b.interactions - a.interactions;
+    });
+
+    res.render("mojipets/profile_most", {
+      title: userObj.username + "'s Profile",
+      uname: userObj.username,
+      display_name: userObj.displayname,
+      profile_pic: userObj.pfp.img,
+      join_date: dateToMMDDYY(userObj.joinDate),
+      last_online: dateToMMDDYY(userObj.lastLogin),
+      most_pets: pets,
       css: "/public/site.css"
     });
   } catch (e) {  // Some db error has occurred
