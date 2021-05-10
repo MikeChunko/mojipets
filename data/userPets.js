b03dea6e746e1e3f3c432c92537202efb386c6fd
Elijah Wendel <ezwendel@gmail.com>
2021-05-10 16:19:13 -0400

    -1 special

--- data/userPets.js
+++ data/userPets.js
@@ -189,8 +189,12 @@ async function feed(body) {
   newPets.push(pet)
 
   owner.pets = newPets
-  if (!owner.foods[foodId] || owner.foods[foodId] - 1 <= 0) throw 'Error: not enough food to feed pet.'
-  owner.foods[foodId] -= 1
+  if (!owner.foods[foodId] || owner.foods[foodId] - 1 <= 0) {
+    if (owner.foods[foodId] != -1) throw 'Error: not enough food to feed pet.'
+  }
+  if (owner.foods[foodId] != -1) {
+    owner.foods[foodId] -= 1
+  }
 
   let updateId = ObjectIdMongo(owner._id)
   delete owner._id
